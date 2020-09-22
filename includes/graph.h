774b89f2991db2229acc8c14773c8ac9c2544d5c
Adolfo Ashara <aashara-@am-j2.msk.21-school.ru>
2020-09-22 20:34:44 +0300

    Add graph.h

---
+++ includes/graph.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graph.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 20:51:26 by aashara           #+#    #+#             */
+/*   Updated: 2020/09/22 20:21:09 by aashara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GRAPH_H
+# define GRAPH_H
+
+# include <stdio.h>
+
+typedef enum
+{
+	False = 0,
+	True = 1,
+}					t_bool;
+
+typedef enum
+{
+	Start,
+	Middle,
+	End
+}					t_node_type; // may be not used
+
+typedef	struct		s_edge
+{
+	size_t			from;
+	size_t			to;
+	int				weight;
+	struct s_edge	*next;
+}					t_edge;
+
+typedef struct		s_node
+{
+	char			*name;
+	int				x;
+	int				y;
+	t_node_type		type; // may be not used
+	t_edge			*edges;
+}					t_node;
+
+typedef struct		s_graph
+{
+	size_t			nb_nodes;
+	size_t			malloc_nodes;// may be not used
+	t_node			**nodes;
+	size_t			graph_start;
+	size_t			graph_end;
+}					t_graph;
+
+#endif
